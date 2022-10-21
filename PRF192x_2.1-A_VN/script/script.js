fb82444032b9e8c58b0af79ee3b500c275cf27db
o0Poseidon0o <lenhan16587@gmail.com>
2022-10-21 16:35:31 +0700

    on tap
    on tap thi het mon

---
+++ PRF192x_2.1-A_VN/script/script.js
@@ -0,0 +1,15 @@
+let js = "amazing";
+console.log(40 + 8 - 23 + 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstname = "Jonas";
+console.log(firstname);
+console.log(js);
+js = "nhan";
+js = "test";
+console.log(js);
+const a = 23;
+
+console.log(a);
