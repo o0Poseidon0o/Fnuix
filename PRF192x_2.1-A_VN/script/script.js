1b623fba405ea9fa9e43164f238a4753a3ec9172
o0Poseidon0o <lenhan16587@gmail.com>
2022-10-25 16:21:02 +0700

    bai4
    bai4

--- PRF192x_2.1-A_VN/script/script.js
+++ PRF192x_2.1-A_VN/script/script.js
@@ -39,3 +39,89 @@ if (population >= 33000000) {
     `${country}'s population is ${33000000 - population} million below average`
   );
 }
+//switch
+const day = "monday";
+switch (day) {
+  case "monday":
+    console.log("ke hoach");
+    console.log("nghi lam");
+    break;
+  case "Tuesday":
+    console.log("Thứ ba");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
+//lab3
+var a = "9" - "5";
+console.log(a);
+var a = Boolean("123" < 57);
+console.log(a);
+
+// const numNeighours = Number(
+//   prompt("How many neighbor countries does your country have?")
+// );
+// // if (numNeighours == 1) {
+// //   console.log("Only 1 border!");
+// // } else if (numNeighours > 1) {
+// //   console.log("More than 1 border");
+// // } else {
+// //   console.log("No borders");
+// // }
+// if (numNeighours === 1) {
+//   console.log("Only 1 border!");
+// } else if (numNeighours > 1) {
+//   console.log("More than 1 border");
+// } else if (numNeighours === 0) {
+//   console.log("No borders");
+// }
+
+const ngonNgu = prompt("Ngôn ngữ là gì");
+const danSo = prompt("Dân số bao nhiêu");
+const daoQuoc = Boolean(prompt("Có phải đảo Quốc không???"));
+
+if (ngonNgu == "English" && danSo == 50 && daoQuoc === true) {
+  console.log("You should live in Portugal:)");
+} else {
+  console.log("Portugal does not meet your criteria :(");
+}
+
+switch (ngonNgu) {
+  case "Chinese or Mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "Spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "English":
+    console.log("3rd place");
+    break;
+  case "Hindi":
+    console.log("Number 4");
+    break;
+  case "Arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
+// Toán tử 3 ngôi
+danSo > 33
+  ? console.log("Portugal's population is above average.")
+  : console.log("Portugal's population is below average.");
+// bat che do strict de tranh loi
+("use strict");
+function logger() {
+  console.log("My name is Nhan");
+}
+logger();
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oragnes.`;
+  return juice;
+}
+fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+const apple0Juice = fruitProcessor(8, 2);
+console.log(apple0Juice);
