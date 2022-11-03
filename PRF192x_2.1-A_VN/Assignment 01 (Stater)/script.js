a12a8f9e1c9b29eb9b98014a2787342fd296879f
o0Poseidon0o <lenhan16587@gmail.com>
2022-11-03 16:21:16 +0700

    as1
    10%

--- PRF192x_2.1-A_VN/Assignment 01 (Stater)/script.js
+++ PRF192x_2.1-A_VN/Assignment 01 (Stater)/script.js
@@ -1,1 +1,43 @@
-'use strict';
+"use strict";
+
+// Toggle class active when click on navbar
+const sidebarTitleEl = document.getElementById("sidebar-title");
+const sidebarEl = document.getElementById("sidebar");
+sidebarTitleEl.addEventListener("click", function () {
+  sidebarEl.classList.toggle("active");
+});
+
+// Khai báo biến để lấy các Element.
+const submitBtn = document.getElementById("submit-btn");
+const idInput = document.getElementById("input-id");
+const nameInput = document.getElementById("input-name");
+const ageInput = document.getElementById("input-age");
+const typeInput = document.getElementById("input-type");
+const weightInput = document.getElementById("input-weight");
+const lengthInput = document.getElementById("input-length");
+const colorInput = document.getElementById("input-color-1");
+const breedInput = document.getElementById("input-breed");
+const vaccinatedInput = document.getElementById("input-vaccinated");
+const dewormedInput = document.getElementById("input-dewormed");
+const sterilizedInput = document.getElementById("input-sterilized");
+// Người dùng bấm vào submit để nhập thông tin thú cưng.
+submitBtn.addEventListener("click", function () {
+  const data = {
+    id: idInput.value,
+    name: nameInput.value,
+    age: parseInt(ageInput.value),
+    type: typeInput.value,
+    weight: weightInput.value,
+    length: lengthInput.value,
+    color: colorInput.value,
+    breed: breedInput.value,
+    vacci: vaccinatedInput.checked,
+    dewo: dewormedInput.checked,
+    sterilized: sterilizedInput.checked,
+  };
+  if (data.id === "") {
+    alert("ID must unique!");
+  } else if (data.age <= 1 && data.age >= 15) {
+    alert("Age must be between 1 and 15!");
+  }
+});
