95334d8906cea5a51358492d407932870fc53570
o0Poseidon0o <lenhan16587@gmail.com>
2022-12-07 16:42:41 +0700

    React

--- Ontap_JS/js/main.js
+++ Ontap_JS/js/main.js
@@ -1,7 +1,55 @@
 "use strict";
 const submit = document.querySelector(".btn-doom");
+const submitapi = document.querySelector(".btn-api");
 const doom = document.querySelector(".doom");
+const api = document.querySelector(".api");
+const tabp = document.getElementsByTagName("p"); //Gọi một phần tử thẻ p
+const subClose = document.querySelector(".btn-closeDoom");
 
+// ----------------------------modal DOOM-------------------------------
 submit.addEventListener("click", function () {
-  doom.classList.remove("hidden");
+  doom.classList.remove("hidden"); //Thêm Class vào 1 Class
 });
+subClose.addEventListener("click", function () {
+  doom.classList.add("hidden");
+});
+// --------------------------Đổi các thẻ p sang màu đen-------------------
+tabp[0].style.color = "black";
+console.log(tabp);
+// --------------modal API--------------------------
+submitapi.addEventListener("click", function () {
+  api.classList.remove("hidden");
+});
+subClose.addEventListener("click", function () {
+  api.classList.add("hidden");
+});
+tabp[1].style.color = "black";
+console.log(tabp);
+// -------------fetch-------------------
+// Trả ra 1 promise
+fetch(
+  `https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly`
+)
+  .then((data) => {
+    return data.json(); //Chuyển đổi sang Json
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+//------------------async await--------------------
+const getNewTodo = async () => {
+  // Phương pháp này dùng để bắt lỗi dòng lệnh
+  //Try là thực hiện dòng lệnh trong khối nếu bị lỗi sẽ đưa ra catch
+  try {
+    let data = await fetch(); //hàm fetch trả về 1 promise
+    let data1 = await data.json();
+    console.log(data1);
+    return data;
+  } catch {
+    console.log();
+  }
+  throw new Error(data.status); // throw dùng để chuyển ra 1 lỗi
+}; //Khi kết nối với data thì nên sử dụng vì khi hành động lấy
+//data nó sẽ trả về 1 promise sẽ làm tốn thời gian lấy đa ta về
+//await là chờ tao lấy dữ liệu.
--- RJS301x_1.0-A_VN/baihoc/bai1/reactcoban/src/App.js
+++ RJS301x_1.0-A_VN/baihoc/bai1/reactcoban/src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./compoments/ExpenseItem";
+import React from "react";
+import Expenses from "./compoments/Expenses";
 
 function App() {
   const expenses = [
@@ -22,32 +23,21 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      <h2>LAB 1</h2>
-      <p>Bài lab làm quen với React</p>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
-    </div>
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started!"),
+    React.createElement(Expenses, { items: expenses })
   );
+  // return (
+  //   <div>
+  //     <div>
+  //       <h2>LAB 1</h2>
+  //       <p>Bài lab làm quen với React</p>
+  //       <Expenses items={expenses} />
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default App;
