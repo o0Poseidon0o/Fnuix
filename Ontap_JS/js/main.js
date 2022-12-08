58ee1b1e5bb510246954dc7b5af1b558315254ec
o0Poseidon0o <lenhan16587@gmail.com>
2022-12-08 16:25:46 +0700

    ontap

--- Ontap_JS/js/main.js
+++ Ontap_JS/js/main.js
@@ -27,15 +27,15 @@ tabp[1].style.color = "black";
 console.log(tabp);
 // -------------fetch-------------------
 // Trả ra 1 promise
-fetch(
-  `https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly`
-)
-  .then((data) => {
-    return data.json(); //Chuyển đổi sang Json
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// fetch(
+//   `https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly`
+// )
+//   .then((data) => {
+//     return data.json(); //Chuyển đổi sang Json
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
 
 //------------------async await--------------------
 const getNewTodo = async () => {
@@ -53,3 +53,59 @@ const getNewTodo = async () => {
 }; //Khi kết nối với data thì nên sử dụng vì khi hành động lấy
 //data nó sẽ trả về 1 promise sẽ làm tốn thời gian lấy đa ta về
 //await là chờ tao lấy dữ liệu.
+
+const Arr1 = new Array(7, 8, 9, 10, 11, 12);
+const Arr = [2, 3, 4, 5];
+
+// ------------------filter loc du lieu---------------------------//
+// Sẽ lọc hết tất cả phần tử trong mảng
+const sochan = Arr.filter((a) => {
+  return a % 2 === 0;
+});
+console.log(sochan);
+// ------------------------find----------------------------//
+// Chỉ trả về kết quả đugns đầu tiên
+const sochan1 = Arr.find((a) => {
+  return a % 2 === 0;
+});
+console.log(sochan1);
+// --------------------map--------------------------------//
+//Thay đổi Array hiện tại và tạo ra 1 Array mới
+const sole = Arr1.map((a) => {
+  return a % 2 !== 0;
+});
+console.log(sole);
+console.log(Arr1);
+// -------------ForEach----------------------//
+// trả về giá trị true false
+const sole1 = Arr1.forEach((a) => {
+  return a % 2 !== 0;
+});
+console.log(sole1);
+//--------------------some-------------------//
+// Kiểm tra xem mảng có phần tử nào thỏa điều kiện không
+// every Kiểm tra tất cả giá trị có giá trị đúng không
+const sole2 = Arr1.some((a) => {
+  return a % 2 !== 0;
+});
+console.log(sole2);
+// // -------------reduce-----------------//
+// // Biến Arry thành Object
+// const sochan2 = Arr1.reduce((acc, cur) => {
+//   //accumulate=>tích lũy qua từng vòng lặp=>acc={}
+//   //cur=>phần tử đã loop qua
+//   return { acc, [cur.email]: cur };
+// }, {});
+// // Biến  Object thành Arr
+// const sochan3 = Arr1.values(Arr1).reduce(
+//   acc,
+//   (cur) => {
+//     // acc=[]
+//     return [acc, cur.email];
+//   },
+//   []
+// );
+const total = Arr1.reduce((acc, cur) => {
+  return acc + cur; //0+7=7=>acc=7+8 V.v....
+}, 0);
+console.log(total);
