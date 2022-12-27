df2f91cff4ca98e97aa98567b54b8b7596064c16
o0Poseidon0o <lenhan16587@gmail.com>
2022-12-27 15:32:17 +0700

    Reaj

---
+++ RJS301x_1.0-A_VN/Assignment01/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBarAs1 from "../../components/navBar/NavBarAs1";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBarAs1></NavBarAs1>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
