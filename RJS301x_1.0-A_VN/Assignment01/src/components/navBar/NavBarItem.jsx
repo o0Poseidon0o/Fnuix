477bd7b759029888bc379003273ce47f0ed2bb08
o0Poseidon0o <lenhan16587@gmail.com>
2022-12-28 10:22:37 +0700

    React
    React

--- RJS301x_1.0-A_VN/Assignment01/src/components/navBar/NavBarItem.jsx
+++ RJS301x_1.0-A_VN/Assignment01/src/components/navBar/NavBarItem.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 // // Đọc file JSON
 // // --------------Cách 1------------------
 // const navBar = require("../../../data/navBar.json");
 // // ------------cách 2------------
 import navBar from "../../data/navBar.json";
-const NavBarItem = (props) => {
-  return navBar.map((item) => <div>{item.type}</div>);
+const NavBarItem = () => {
+  return navBar.map((item) => (
+    <div key={item.style}>
+      <div>{item.icon}</div>
+
+      <div>{item.type}</div>
+    </div>
+  ));
 };
 export default NavBarItem;
--- RJS301x_1.0-A_VN/Assignment01/src/components/navBar/NavBarMain.jsx
+++ RJS301x_1.0-A_VN/Assignment01/src/components/navBar/NavBarMain.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import NavBarItem from "./NavBarItem";
+
 function NavBarmain() {
-  return <div>Dy la menu</div>;
+  return (
+    <div className="container-fluid bg-primary">
+      <div className="row">
+        <div className="col-md-9">BOOKING WEBSITE</div>
+        <div className="col-md-3">
+          <button>Registered</button>
+          <button>Login</button>
+        </div>
+      </div>
+      <div className="row">
+        <NavBarItem></NavBarItem>
+      </div>
+    </div>
+  );
 }
 export default NavBarmain;
