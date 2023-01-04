41ed71e5056a3e713ae3021270d055a4de25d0cb
o0Poseidon0o <lenhan16587@gmail.com>
2023-01-04 09:56:28 +0700

    React
    as1

--- RJS301x_1.0-A_VN/Assignment01/src/components/navBar/NavBarItem.jsx
+++ RJS301x_1.0-A_VN/Assignment01/src/components/navBar/NavBarItem.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 // // Đọc file JSON
 // // --------------Cách 1------------------
 // const navBar = require("../../../data/navBar.json");
@@ -8,10 +7,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import navBar from "../../data/navBar.json";
 const NavBarItem = () => {
   return navBar.map((item) => (
-    <div key={item.style}>
-      <div>{item.icon}</div>
-
-      <div>{item.type}</div>
+    <div className="col" key={item.style}>
+      <FontAwesomeIcon icon="fa-solid fa-car" />
+      <FontAwesomeIcon icon={`fa-solid ${item.icon}`}></FontAwesomeIcon>
+      {item.type}
     </div>
   ));
 };
--- RJS301x_1.0-A_VN/Assignment01/src/components/navBar/NavBarMain.jsx
+++ RJS301x_1.0-A_VN/Assignment01/src/components/navBar/NavBarMain.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import NavBarItem from "./NavBarItem";
-
+import "./NavBarMain.css";
 function NavBarmain() {
   return (
     <div className="container-fluid bg-primary">
       <div className="row">
-        <div className="col-md-9">BOOKING WEBSITE</div>
-        <div className="col-md-3">
-          <button>Registered</button>
-          <button>Login</button>
+        <div className="col-md-9 text-light fs-2">BOOKING WEBSITE</div>
+        <div className="col-md-3 ">
+          <button className="btn btn-light m-1 text-primary">Registered</button>
+          <button className="btn btn-light m-1 text-primary">Login</button>
         </div>
       </div>
-      <div className="row">
+      <div className="row text-light">
         <NavBarItem></NavBarItem>
+        <div>
+          <h1>A lifetime of discounts? It's Genius</h1>
+          <p>
+            Get rewarded for your travels - unlock instant savings of 10% or
+            more with a free account
+          </p>
+          <button className="btn btn-primary">Sign/Register</button>
+        </div>
       </div>
     </div>
   );
