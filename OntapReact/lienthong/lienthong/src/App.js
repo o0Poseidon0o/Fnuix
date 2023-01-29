44abdcc3761d59c19882c37bd524e2c29bda5997
o0Poseidon0o <90851953+o0Poseidon0o@users.noreply.github.com>
2023-01-29 21:00:13 +0700

    lienthong
    React

---
+++ OntapReact/lienthong/lienthong/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tongquan from './components/tongquaninternet/tongquan';
+
+
+function App() {
+  return (
+    <div>
+      <Tongquan></Tongquan>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ OntapReact/lienthong/lienthong/src/components/tongquaninternet/tongquan.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tongquan = ()=>{
+    return(
+        <div className="container-fluid">
+            <h1>Tong quan internert</h1>
+        </div>
+        
+            
+        
+    );
+}
+export default Tongquan
