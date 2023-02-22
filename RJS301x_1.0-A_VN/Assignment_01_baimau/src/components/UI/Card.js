edf7998371e91b761ea0742188ea3a9bbd1a5bc0
o0Poseidon0o <lenhan16587@gmail.com>
2023-02-22 07:45:15 +0700

    ReactJS
    React

--- RJS301x_1.0-A_VN/Assignment_01_baimau/src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import "./Card.css";
-
-// component các thẻ có border radius
-const Card = (props) => {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
-export default Card;
---
+++ RJS301x_1.0-A_VN/lab_review/lab7.1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Component/Card";
+import Button from "./Component/Button";
+function App() {
+  return (
+    <div>
+      <Card>
+        <Button></Button>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
