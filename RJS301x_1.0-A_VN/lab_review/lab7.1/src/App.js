6dbaa76735680c4da24864a56faff80050c512ea
o0Poseidon0o <lenhan16587@gmail.com>
2023-02-22 14:05:07 +0700

    React
    React

--- RJS301x_1.0-A_VN/lab_review/lab7.1/src/App.js
+++ RJS301x_1.0-A_VN/lab_review/lab7.1/src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
-import Card from "./Component/Card";
-import Button from "./Component/Button";
+import React, { useState } from "react";
+import AddUser from "./Component/Users/AddUser";
+import UsersList from "./Component/Users/UsersList";
+
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-      <Card>
-        <Button></Button>
-      </Card>
+      <AddUser onAddUser={addUserHandler}></AddUser>
+      <UsersList users={usersList}></UsersList>
     </div>
   );
 }
