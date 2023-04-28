1e824e9efaf049ca07af795b11957fc3ade64f1f
o0Poseidon0o <lenhan16587@gmail.com>
2023-04-28 16:07:24 +0700

    React
    as3

---
+++ RJS301x_1.0-A_VN/baihoc/Cơ_ban_can_nam/state.js
@@ -0,0 +1,15 @@
+function Counter() {
+  // State lưu bộ đếm với biến counter
+  // setCounter hàm lưu giá trị cho biến counter
+  const [counter, setCounter] = useState(0);
+  //Hàm sự kiên cho bộ đếm
+  const increment = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+  return (
+    <div>
+      Value:{counter}
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
