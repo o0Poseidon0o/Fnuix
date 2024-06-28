ebc1f8ccbb7aca29145f85e6d8fe152b90141092
o0Poseidon0o <90851953+o0Poseidon0o@users.noreply.github.com>
2024-06-28 16:15:10 +0700

    NodeJs

--- NodeJS/part2/app.js
+++ NodeJS/part2/app.js
@@ -1,41 +1,11 @@
-const http=require('http');
-const fs =require('fs')
+const http = require('http');//Dòng này sử dụng mô-đun HTTP của Node.js để tạo một máy chủ HTTP.
 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method
-    if (url=== '/'){
-        
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url==='/message'&& method==='POST'){
-        const body=[];
-        req.on('data', (chunk)=>{
-            console.log(chunk)
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-            const parsedBody =Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message =parsedBody.split('=') [1];
-            fs.writeFileSync('message.txt', message)
-        })
-        
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-    // console.log(req.url,req.method,req.headers)
-    // process.exit();
-    res.setHeader('content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title><head>');
-    res.write('<body><h1>Hello from my Node.Js Server<h1></body>');
-    res.write('</html>');
-    res.end();
-})
-server.listen(3000);
+const routes = require('./routes'); //Dòng này yêu cầu tệp routes.js và gán giá trị trả về cho biến routes. Trong trường hợp này, routes.js chứa mã xử lý yêu cầu và một số văn bản cố định.
+
+console.log(routes.someText);//Dòng này in ra console giá trị của thuộc tính someText từ routes. Nếu bạn nhớ đoạn mã trong routes.js, giá trị của someText là "Some hard coded text".
+
+const server = http.createServer(routes.handler);//Dòng này tạo một máy chủ HTTP và sử dụng hàm xử lý yêu cầu handler từ routes.js.
+// Khi máy chủ nhận được một yêu cầu, nó sẽ chuyển yêu cầu đó đến hàm handler trong routes.js.
+
+server.listen(3000);//Dòng này khởi động máy chủ và lắng nghe các kết nối trên cổng 3000. 
+//Khi bạn truy cập http://localhost:3000 trong trình duyệt, máy chủ sẽ xử lý các yêu cầu theo logic đã định nghĩa trong handler.
